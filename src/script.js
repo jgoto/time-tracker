8ba68fc89b3070ddf7ce992a1f80a5029d9f5402
Jin Nan Goto <43223423+jgoto@users.noreply.github.com>
2024-10-08 22:30:46 -0400

    Added export button which exports the tasks to local storage.

--- src/script.js
+++ src/script.js
@@ -2,13 +2,13 @@ const lightModeBtn = document.getElementById("light-mode-btn");
 const darkModeBtn = document.getElementById("dark-mode-btn");
 const trackBtn = document.getElementById("track-btn");
 const taskInput = document.getElementById("task-input");
+const exportBtn = document.getElementById("export-btn");
 
 class TimeTracker{
     constructor(){
         this.trackingItem=false;
         this.tasks = [];
         this.taskCounter = document.getElementById("task-counter");
-        this.trackedIndex = "";
         this.timer=0;
         this.currentTime = new Date();
         this.startTracker();
@@ -50,11 +50,19 @@ class TimeTracker{
         this.tasks.forEach(task =>{
             if(task.enabled){
                 task.enabled = false;
-                task.elapsedTime=Math.floor((this.currentTime-task.startTime)/1000)
                 console.log(task)
             }
         })
         this.trackingItem=false;
+    }
+
+    exportTasks(){
+        if(this.tasks.length>0){
+            localStorage.setItem("tasks", JSON.stringify(this.tasks))
+        }
+        else{
+            console.log("No tasks to save");
+        }
     }
 }
 
@@ -88,7 +96,7 @@ trackBtn.addEventListener("click", ()=>{
     if(!timeTracker.trackingItem && taskInput.value!=="")
     {
         trackBtn.classList.remove("btn-success");
-        trackBtn.classList.add("btn-danger");
+        trackBtn.classList.add("btn-info");
         trackBtn.innerText="Stop";
         timeTracker.trackItem({
             id: "1",
@@ -101,7 +109,7 @@ trackBtn.addEventListener("click", ()=>{
     }
     else
     {
-        trackBtn.classList.remove("btn-danger");
+        trackBtn.classList.remove("btn-info");
         trackBtn.classList.add("btn-success");
         trackBtn.innerText="Track";
         trackingItem=false;
@@ -110,3 +118,6 @@ trackBtn.addEventListener("click", ()=>{
     }
 })
 
+exportBtn.addEventListener("click", ()=>{
+    timeTracker.exportTasks();
+})
